88e7bf3a3cfaea51a81d625857f179c45ebddeb1
SheiKhaAhm laptop <itzshakeey@gmail.com>
2023-02-28 15:14:48 +0000

    add cors implementation

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const fruits = require('./fruits.json');
-// console.log(fruits)
-
 const port = process.env.port;
 
+app.use(cors());
 app.use(express.json());
 
 //home route
