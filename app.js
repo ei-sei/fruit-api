2dd3e3d9bdedc7cb8f7ce3d6c68d4e9a8d5d70f9
SheiKha Ahm desk <itzshakeey@gmail.com>
2023-03-01 21:35:13 +0000

    fixed post route and added logger

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const fruits = require('./fruits.json');
+const cors = require("cors");
+const express = require("express");
 
+const app = express();
+const fruits = require("./fruits.json");
+const logger = require("./logger")
+
+app.use(logger);
 app.use(cors());
 app.use(express.json());
 
@@ -10,40 +13,43 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('hello, fruity API');
 
-})
+});
 
 //return all the fruits route
 app.get('/fruits', (req, res) => {
     res.status(200).send(fruits)
-})
+});
 
 //return a single fruit route
 //what if the fruit is not found?
 //what if the user submits a fruit with no capital letters
-
-app.get(`/fruits/:fruitName`, (req, res) => {
-    const fruitName = req.params.fruitName.toLowerCase();
-
-    const fruit = fruits.find((fruit) => fruit.name.toLowerCase() == fruitName);
+app.get("/fruits/:name", (req, res, next) => {
+    const name = req.params.name.toLowerCase();
+    const fruit = fruits.find((fruit) => fruit.name.toLowerCase() == name);
 
     if (fruit == undefined) {
-        res.status(404).send();
+        // res.status(404).send(); 
+        next();
     } else {
         res.send(fruit);
     }
 })
 
+const ids = fruits.map((fruit) => fruit.id);
+let maxID = Math.max(...ids);
 
 app.post("/fruits", (req, res) => {
-
     //check that the fruit doesn't exist in the data
     const fruit = fruits.find((fruit) => fruit.name.toLowerCase() === req.body.name.toLowerCase);
 
     if(fruit != undefined){
-        res.status(409).send();
+        res.status(409).send(); //if item exists; sends conflict response code
     }
     else{
-        fruits.push(req.body);
+        maxID += 1;
+        req.body.id = maxID;
+        
+        fruits.push(req.body);  //adds the fruit to the list #note: this does not modify to json
 
         res.status(201).send(req.body);
     }
